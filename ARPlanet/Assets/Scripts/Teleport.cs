220fd81f6a7d24e9d9b09ec6df6d073dffa6b729
Chrismed68 <42751189+Chrismed68@users.noreply.github.com>
2020-12-16 01:24:17 -0500

    Final Push From Chris
    I added music, fixed grammar, very stuble but meaningful changes

---
+++ ARPlanet/Assets/Scripts/Teleport.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+public class Teleport : MonoBehaviour
+{
+    
+    void OnCollisionEnter(Collision bye)
+    {
+        if (bye.gameObject.name == "Rocketship")
+        {
+            Debug.Log("Collision Detected");
+            Destroy(bye.gameObject);
+        }
+    }
+}
